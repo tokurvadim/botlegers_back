9d63f19a25edbc44b85c4428fd339771ac93dd03
tokurvadim <tchikhrinoff.vadim@yandex.ru>
2024-05-20 10:18:09 +0300

    change urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import GetListCreateFormAPIView, GetButtonsListAPIView,GetListCreateBriefAPIView, GetListCreateAboutUsAPIView
+from .views import GetListCreateFormAPIView, GetButtonsListAPIView,GetListCreateBriefAPIView, GetListCreateAboutUsAPIView, GetListCreatePortfolioAPIView
 
 urlpatterns = [
     path('form/', GetListCreateFormAPIView.as_view()),
     path('list/', GetButtonsListAPIView.as_view()),
     path('brief/', GetListCreateBriefAPIView.as_view()),
-    path('aboutus/', GetListCreateAboutUsAPIView.as_view())
+    path('aboutus/', GetListCreateAboutUsAPIView.as_view()),
+    path('portfolio/', GetListCreatePortfolioAPIView.as_view())
 ]
